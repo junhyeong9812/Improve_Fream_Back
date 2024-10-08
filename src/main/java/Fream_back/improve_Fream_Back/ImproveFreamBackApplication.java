d95bdc3883917a5090f45e77badb47a46742cd4e
jun <pickjog@gmail.com>
2024-10-08 23:05:50 +0900

    first commit

---
+++ src/main/java/Fream_back/improve_Fream_Back/ImproveFreamBackApplication.java
@@ -0,0 +1,13 @@
+package Fream_back.improve_Fream_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImproveFreamBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImproveFreamBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fream_back/improve_Fream_Back/ImproveFreamBackApplicationTests.java
@@ -0,0 +1,13 @@
+package Fream_back.improve_Fream_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproveFreamBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
